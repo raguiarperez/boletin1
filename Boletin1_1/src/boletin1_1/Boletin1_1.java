18f5fcb98eb90586024fe1f93bcbd319c232e7e0
raguiarperez <raguiarperez@nautilus8.danielcastelao.org>
2018-10-09 09:10:08 +0200

    boletin 1.1

---
+++ Boletin1_1/src/boletin1_1/Boletin1_1.java
@@ -0,0 +1,16 @@
+
+package boletin1_1;
+
+
+public class Boletin1_1 {
+
+
+    public static void main(String[] args) {
+        
+        int base=4,altura=3,areatriangulo;
+        areatriangulo=base*altura/2;
+        System.out.println(areatriangulo);
+               
+    }
+    
+}
