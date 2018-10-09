0ec98b6752eef59a3162d0d97f10095ac753edda
raguiarperez <raguiarperez@nautilus8.danielcastelao.org>
2018-10-09 10:50:50 +0200

    boletin 1.4

--- Boletin1_4/src/boletin1_4/Boletin1_4.java
+++ Boletin1_4/src/boletin1_4/Boletin1_4.java
@@ -9,10 +9,16 @@ public class Boletin1_4 {
         Scanner ler= new Scanner(System.in);
         float suma,resta,producto,cociente;
         System.out.println("introducir valor X");
-        x=ler.nextFloat();
+        float x=ler.nextFloat();
         System.out.println("introducir valor Y");
-        Y= ler.nextFloat();
+        float Y= ler.nextFloat();
         
+        suma=x+Y;
+        resta=x+Y;
+        producto=x*Y;
+        cociente=x/Y;
+        
+       System.out.println(" suma:"+suma+"\n resta"+resta+"\n producto"+producto+"\n cociente"+cociente);
         
     }
     
