365cbcf244d3225a6685132ac488942ddd48591f
raguiarperez <raguiarperez@nautilus8.danielcastelao.org>
2018-10-09 09:10:17 +0200

    boletin 1.2

---
+++ Boletin1_2/src/boletin1_2/Boletin1_2.java
@@ -0,0 +1,14 @@
+
+package boletin1_2;
+
+
+public class Boletin1_2 {
+
+
+    public static void main(String[] args) {
+        int lado=3,areacuadrado;
+        areacuadrado=lado*lado;
+        System.out.println("areacuadrado = "+areacuadrado);
+    }
+    
+}
