c805d1273e8f2163d5dc4e69932ae26634fd36d7
raguiarperez <raguiarperez@nautilus8.danielcastelao.org>
2018-10-09 09:08:54 +0200

    boletin 1.4

---
+++ Boletin1_4/src/boletin1_4/Boletin1_4.java
@@ -0,0 +1,19 @@
+
+package boletin1_4;
+
+import java.util.Scanner;
+
+public class Boletin1_4 {
+
+    public static void main(String[] args) {
+        Scanner ler= new Scanner(System.in);
+        float suma,resta,producto,cociente;
+        System.out.println("introducir valor X");
+        x=ler.nextFloat();
+        System.out.println("introducir valor Y");
+        Y= ler.nextFloat();
+        
+        
+    }
+    
+}
